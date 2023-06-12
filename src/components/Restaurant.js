f62a31424e73c33004eb917695756b4c0a0cf43e
Kendall Holmes <98422018+kendallatada@users.noreply.github.com>
2023-06-12 15:01:50 -0700

    Components Livecode 6/12

---
+++ src/components/Restaurant.js
@@ -0,0 +1,17 @@
+import "./Restaurant.css";
+
+const Restaurant = () => {
+  const restaurantName = "This is Salty's";
+  return (
+    <div>
+      <h2 className="restaurant-title"> {restaurantName} </h2>
+      <ul>
+        <li> Cuisine: Seafood </li>
+        <li> Rating: 4.8 </li>
+        <li> Distance from Ada: 5 miles</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Restaurant;
