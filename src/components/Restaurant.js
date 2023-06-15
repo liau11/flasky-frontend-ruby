7c0e942966a81cb9c4eadb0d6a61ea0fcd514537
nancy-harris <nancy.harris@adadevelopersacademy.org>
2023-06-15 11:18:34 -0700

    Add state to Restaurant

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,5 +1,6 @@
 import "./Restaurant.css";
 import PropTypes from "prop-types";
+import { useState } from 'react';
 
 // const Restaurant = (props) => {
 //   const restaurantName = props.name;
@@ -7,15 +8,34 @@ import PropTypes from "prop-types";
 //   const rating = props.rating;
 //   const distance = props.distance;
 
-const Restaurant = ({ name, cuisine, rating, distance, popularity }) => {
+const Restaurant = ({ name, cuisine, rating, distance }) => {
+  const [updateRating, setRating] = useState(rating);
+
+  const changeRating = (direction) => {
+    if (direction === 'up') {
+      setRating(updateRating + 1);
+    } else if (direction === 'down') {
+      setRating(updateRating - 1);
+    }
+  };
+
+  const determineColor = () => {
+    if (updateRating > 5) {
+      return "high-rating";
+    } else {
+      return "rating";
+    }
+  };
+
   return (
     <div>
       <h2 className="restaurant-title"> {name} </h2>
       <ul>
         <li> Cuisine: {cuisine} </li>
-        <li> Rating: {rating} </li>
+        <li className={determineColor()}> Rating: {updateRating} </li>
+        <button onClick={() => {changeRating('up')}}>Upvote</button>
+        <button onClick={() => {changeRating('down')}}>Downvote</button>
         <li> Distance from Ada: {distance} </li>
-        <li> Popularity: {popularity} </li>
       </ul>
     </div>
   );
